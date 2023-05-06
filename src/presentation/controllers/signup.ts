339c5c8ed0ce88ffb7e3e5eebb6913793fc00f75
thivalls <olavalls@gmail.com>
2023-05-06 17:33:51 -0300

    refactor: reduce importation put all exports together in index file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,6 @@
-import { HttpRequest, HttpResponse } from '../protocols/http'
-import { MissingParamError } from '../errors/missing-param-error'
+import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
+import { MissingParamError, InvalidParamError } from '../errors'
 import { badResponse, serverError } from '../helpers/http-helper'
-import { Controller } from '../protocols/controller'
-import { EmailValidator } from '../protocols/email-validator'
-import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
