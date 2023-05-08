d8347373a7429636989d35127fae2581837a82fa
thivalls <olavalls@gmail.com>
2023-05-07 21:42:49 -0300

    test: ensure EmailValidatorAdapter returns correct validation using validator lib

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,23 @@
-import { EmailValidatorAdapter } from './email-validator.adapter'
+import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('Email Validator Adapter', () => {
   test('Should return false if EmailValidator returns false', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@email.com')
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if EmailValidator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@email.com')
+    expect(isValid).toBe(true)
   })
 })
