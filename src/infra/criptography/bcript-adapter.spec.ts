c748c93f430aca789b11788bb826aec0c588f293
thivalls <olavalls@gmail.com>
2023-05-09 07:23:17 -0300

    test: ensure BcryptAdapter throws when encrypter library throws

--- src/infra/criptography/bcript-adapter.spec.ts
+++ src/infra/criptography/bcript-adapter.spec.ts
@@ -29,4 +29,14 @@ describe('Bcrypt Adapter', () => {
 
     expect(hash).toBe('hash')
   })
+
+  test('Should return 500 if BcryptAdapter throws', async () => {
+    const sut = makeSut()
+
+    jest.spyOn(sut, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+
+    const promise = sut.encrypt('any_value')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
